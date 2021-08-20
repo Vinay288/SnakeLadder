594f2392326fc69a465da17565b92d67c005662a
Vinay288 <vinayhiremath288@gmail.com>
2021-08-20 11:59:19 +0530

    added snake and player play feature

--- src/SnakeLadder.java
+++ src/SnakeLadder.java
@@ -1,8 +1,20 @@
 
 public class SnakeLadder {
 	public static void main(String [] args) {
-		int n = 1+(int)(Math.random()*6);
-		System.out.println(n);
+		int player1_position = 0;
+		int die_number = 1 + (int) (Math.random() * 6);
+		int play_check = (int)(Math.random()*3);
+		if(play_check==0)
+			System.out.println("NO PLAY");
+		else if(play_check == 1) {
+			System.out.println("Player Move");
+			player1_position+=die_number;
+		}
+		else {
+			System.out.println("Snake play");
+			player1_position=0>player1_position-die_number?0:player1_position-die_number;
+		}
+		System.out.println("Player's position is "+player1_position);
 	}
 
 }
